ebae257fe512b1537b65b86a8ba0b72825211d3c
Rafał Nater <r.nater@merixstudio.com>
2022-05-05 10:49:11 +0200

    Enable JWT authentication

--- src/user/repositories.py
+++ src/user/repositories.py
@@ -1,6 +1,7 @@
 from typing import Type
 
 from core.repository import AbstractRepository
+from core.security import password_hashing
 from user.mappers import UserCreationMapper
 from user.models import User as UserModel
 from user.schemas import User as UserSchema
@@ -12,3 +13,14 @@ class UserRepository(AbstractRepository[UserModel, int, UserSchema]):
 
     def _get_mapper(self) -> Type[UserCreationMapper]:
         return UserCreationMapper
+
+    def get_user(self, email: str):
+        return self._db.query(self._get_model_class()).filter_by(email=email).first()
+
+    def authenticate_user(self, email: str, password: str):
+        user = self.get_user(email=email)
+        if not user:
+            return False
+        if not password_hashing.verify_hash(password=password, hash=user.hashed_password):
+            return False
+        return user
--- src/user/schemas.py
+++ src/user/schemas.py
@@ -15,3 +15,8 @@ class User(UserBase):
 
     class Config:
         orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
